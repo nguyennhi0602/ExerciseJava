ae372cc9245a23acc7f79b60075985c72e326202
nhi <nguyenthinhi0602@gmail.com>
2019-05-14 14:14:03 +0700

    test

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DemoTest {
+    @Test
+    public  void test_sum(){
+        assertEquals(new Demo().sum(2,5),7);
+    }
+    @Test
+    public void test_sub(){
+        assertEquals(new Demo().sub(4,1),3);
+    }
+    @Test
+    public void test_multiple(){
+        assertEquals(new Demo().multiple(9,3),27);
+    }
+}
