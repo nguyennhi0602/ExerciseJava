a1def0c3b505838bf8a9de1d45a007d7c131daff
nhi <nguyenthinhi0602@gmail.com>
2019-05-15 09:01:43 +0700

    Exercise2edit

--- src/test/java/Exercise2Test.java
+++ src/test/java/Exercise2Test.java
@@ -4,13 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 public class Exercise2Test {
     @Test
-    public void test_isPrimeNumber(){
-        assertEquals(new Exercise2().isPrimeNumber(1),false);
-        assertEquals(new Exercise2().isPrimeNumber(12),false);
-    }
-    @Test
     public void test_calculateSumOfNumber(){
-        assertEquals(new Exercise2().calculateSumOfNumber(12),0);
         assertEquals(new Exercise2().calculateSumOfNumber(13),4);
     }
 }
