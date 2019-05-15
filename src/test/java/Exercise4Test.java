a447b8e791f3412cd3699c8217cb18fef4318769
nhi <nguyenthinhi0602@gmail.com>
2019-05-15 17:11:49 +0700

    wip

---
+++ src/test/java/Exercise4Test.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Exercise4Test {
+    @Test
+    public void test_analysisPrimeNumber(){
+        //assertEquals(new Exercise4().analysisPrimeNumber(600),"2^3 * 3 * 5^2");
+        //assertEquals(new Exercise4().analysisPrimeNumber(7),"7");
+        assertEquals(new Exercise4().analysisPrimeNumber(9),"3*3");
+    }
+
+}
