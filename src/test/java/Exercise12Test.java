85e02de4f31e2c703311ebdfe1ebd9776f96e661
nhi <nguyenthinhi0602@gmail.com>
2019-05-16 16:59:55 +0700

    Exercise12Edit2

--- src/test/java/Exercise12Test.java
+++ src/test/java/Exercise12Test.java
@@ -8,7 +8,7 @@ public class Exercise12Test {
         assertEquals(new Exercise12().calculateFactorial(1), 1);
         assertEquals(new Exercise12().calculateFactorial(5), 120);
     }
-    
+
     @Test
     public void test_calculateSum() {
         assertEquals(new Exercise12().calculateSum(3), 2.67, 0.01);
