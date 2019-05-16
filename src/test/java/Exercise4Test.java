2dc1724cd3f847cfddd4c6926d58edb07b62ef48
Võ Quang Hòa <voquanghoa@gmail.com>
2019-05-16 22:34:37 +0700

    Update Exercise4Test.java

--- src/test/java/Exercise4Test.java
+++ src/test/java/Exercise4Test.java
@@ -6,9 +6,6 @@ public class Exercise4Test {
     @Test
     public void test_analysisPrimeNumber(){
         assertEquals(new Exercise4().analysisPrimeNumber(9),"3*3");
-        assertEquals(new Exercise4().analysisPrimeNumber(600),"2^3 * 3 * 5^2");
-        assertEquals(new Exercise4().analysisPrimeNumber(9),"3^2");
-
     }
 
 }
