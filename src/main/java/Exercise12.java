ce131fb7d21865216b9ab6e516705cdba5d5033f
nhi <nguyenthinhi0602@gmail.com>
2019-05-16 17:47:16 +0700

    Exercise12Edit2

--- src/main/java/Exercise12.java
+++ src/main/java/Exercise12.java
@@ -1,16 +1,15 @@
 public class Exercise12 {
-    public int calculateFactorial(int number) {
-        int result = 1;
-        for (int i = 2; i <= number; i++) {
-            result *= i;
-        }
-        return result;
-    }
-
     public double calculateSum(int number) {
-        double result = 1;
-        for (int i = 1; i <= number; i++) {
-            result += (double) 1 / calculateFactorial(i - 1) / i;
+        if (number == 0) {
+            return 1;
+        }
+        double result = 2;
+        for (int i = 2; i <= number; i++) {
+            int calculateFactorial = 1;
+            for (int j = 1; j < i; j++) {
+                calculateFactorial *= j;
+            }
+            result += (double) 1 / calculateFactorial / i;
         }
         return result;
     }
--- src/test/java/Exercise12Test.java
+++ src/test/java/Exercise12Test.java
@@ -4,13 +4,8 @@ import org.junit.Test;
 
 public class Exercise12Test {
     @Test
-    public void test_caculateFactorial() {
-        assertEquals(new Exercise12().calculateFactorial(1), 1);
-        assertEquals(new Exercise12().calculateFactorial(5), 120);
-    }
-
-    @Test
     public void test_calculateSum() {
+        assertEquals(new Exercise12().calculateSum(0), 1, 0.01);
         assertEquals(new Exercise12().calculateSum(3), 2.67, 0.01);
     }
 }
