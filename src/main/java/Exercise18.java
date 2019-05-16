d0b54fa1d077d30606115b47479fd70058a76626
nhi <nguyenthinhi0602@gmail.com>
2019-05-16 22:15:31 +0700

    Exercise18Edit2

--- src/main/java/Exercise18.java
+++ src/main/java/Exercise18.java
@@ -1,14 +1,10 @@
 public class Exercise18 {
     public int getDecimalByDecimal(int number) {
-        String convertBinaryResult = "";
-        while (number > 0) {
-            convertBinaryResult += (number % 2) + "";
-            number /= 2;
-        }
         int result = 0;
-        for (int i = convertBinaryResult.length() - 1; i >= 0; i--) {
-            double temp = Math.pow(2, convertBinaryResult.length() - 1 - i);
-            result += Integer.parseInt(String.valueOf(convertBinaryResult.charAt(i))) * temp;
+        while (number != 0) {
+            result <<= 1;
+            result |= number & 1;
+            number >>= 1;
         }
         return result;
     }
