9bf6b7a24d3c29dff1185143f91ad5e55cefac32
nhi <nguyenthinhi0602@gmail.com>
2019-05-17 15:47:26 +0700

    Exercise12Edit3

--- src/main/java/Exercise12.java
+++ src/main/java/Exercise12.java
@@ -1,15 +1,10 @@
 public class Exercise12 {
     public double calculateSum(int number) {
-        if (number == 0) {
-            return 1;
-        }
-        double result = 2;
-        for (int i = 2; i <= number; i++) {
-            int calculateFactorial = 1;
-            for (int j = 1; j < i; j++) {
-                calculateFactorial *= j;
-            }
-            result += (double) 1 / calculateFactorial / i;
+        double current = 1;
+        double result = 1;
+        for (int i = 1; i <= number; i++) {
+            current /= i;
+            result += current;
         }
         return result;
     }
