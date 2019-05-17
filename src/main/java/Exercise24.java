72274761b42363668002d5c935d097f6c5b3b757
nhi <nguyenthinhi0602@gmail.com>
2019-05-17 16:42:11 +0700

    Exercise 24 Edit 4

--- src/main/java/Exercise24.java
+++ src/main/java/Exercise24.java
@@ -3,12 +3,15 @@ public class Exercise24 {
         if (number == 0) {
             return 0;
         }
-        int a0 = 1, a1 = 1, a3 = 2, count = 2;
-        while (a3 <= number) {
-            a3 = a0 + a1;
+        int a0 = 1, a1 = 1, a2 = 2, count = 2;
+        while ((a2 <= number)) {
+            a2 = a0 + a1;
             a0 = a1;
-            a1 = a3;
+            a1 = a2;
             count++;
+            if (a2 > number) {
+                count--;
+            }
         }
         return count;
     }
