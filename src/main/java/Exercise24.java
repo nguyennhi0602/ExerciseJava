f8cd9020ff528f8b904238a43a4527fe287ab73d
nhi <nguyenthinhi0602@gmail.com>
2019-05-17 10:44:33 +0700

    Exercise24Edit

--- src/main/java/Exercise24.java
+++ src/main/java/Exercise24.java
@@ -1,19 +1,25 @@
 public class Exercise24 {
     public int caculateFibonaci(int number) {
+        if (number == 0) {
+            return 0;
+        }
         if (number == 1 || number == 2) {
             return 1;
-        } else {
-            return caculateFibonaci(number - 1) + caculateFibonaci(number - 2);
         }
+        int count = 2;
+        int a0 = 1, a1 = 1, result = 2;
+        while (count++ < number) {
+            result = a0 + a1;
+            a0 = a1;
+            a1 = result;
+        }
+        return result;
     }
 
     public int countFibonaciNumber(int number) {
-        int result = 1;
-        int FibonaciNumber = 1;
-        while (FibonaciNumber <= number) {
-            result++;
-            FibonaciNumber = caculateFibonaci(result);
+        int result = 0;
+        while (caculateFibonaci(++result)<=number){
         }
-        return result - 1;
+        return result;
     }
 }
--- src/test/java/Exercise24Test.java
+++ src/test/java/Exercise24Test.java
@@ -6,12 +6,13 @@ public class Exercise24Test {
     @Test
     public void test_calculateFibonaci() {
         assertEquals(new Exercise24().caculateFibonaci(1), 1);
+        assertEquals(new Exercise24().caculateFibonaci(0), 0);
         assertEquals(new Exercise24().caculateFibonaci(3), 2);
     }
 
     @Test
     public void test_countFibonaciNumber() {
-        assertEquals(new Exercise24().countFibonaciNumber(2), 3);
-        assertEquals(new Exercise24().countFibonaciNumber(1), 2);
+        assertEquals(new Exercise24().countFibonaciNumber(3), 5);
+        assertEquals(new Exercise24().countFibonaciNumber(0), 1);
     }
 }
