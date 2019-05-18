061e52ef9818b2f60ba22f2c70fe40c95c2eff80
nhi <nguyenthinhi0602@gmail.com>
2019-05-18 09:11:32 +0700

    Exercise 26 Edit

--- src/main/java/Exercise26.java
+++ src/main/java/Exercise26.java
@@ -1,5 +1,9 @@
 public class Exercise26 {
     public int calculateCrazyFibonaci(int a0, int a1, int a2, int a3, int a4, int n) {
+        int[] temp = {a0, a1, a2, a3, a4};
+        if (n < 5) {
+            return temp[n - 1];
+        }
         int count = 5;
         int result = 0;
         while (count < n) {
--- src/test/java/Exercise26Test.java
+++ src/test/java/Exercise26Test.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 public class Exercise26Test {
     @Test
     public void test_calculateCrazyFibonaci(){
+        assertEquals(new Exercise26().calculateCrazyFibonaci(6,2,2,1,4,2),2);
         assertEquals(new Exercise26().calculateCrazyFibonaci(6,2,2,1,4,8),6);
     }
 }
