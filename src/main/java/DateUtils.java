03da3080769d22567c1d3b62dab344d7a8b55915
nhi <nguyenthinhi0602@gmail.com>
2019-05-20 10:37:11 +0700

    Date Utils

--- src/main/java/DateUtils.java
+++ src/main/java/DateUtils.java
@@ -3,14 +3,49 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateUtils {
-    public static Date convertDate(String input, SimpleDateFormat sdf) {
+
+    public static SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yyyy");
+
+
+    public static Date convertToDate(String input, SimpleDateFormat sdf) {
         Date date = null;
         sdf.setLenient(false);
         try {
             date = sdf.parse(input);
-        } catch (ParseException e) {
-            e.getMessage();
+        } catch (ParseException p) {
         }
         return date;
+    }
+    public static boolean isYear(int year) {
+        return year >= 1900 && year <= 3000;
+    }
+
+    public static boolean isMonth(int month) {
+        return month >= 1 && month <= 12;
+    }
+
+    public static boolean isDay(int day, int numberOfDayInMonth) {
+        return day >= 1 && day <= numberOfDayInMonth;
+    }
+
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0)||year % 400 == 0 ;
+    }
+
+    public static int getDayInMonth(int month, int year) {
+        int[] daysOfMonths = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int day = daysOfMonths[month-1];
+        if(month==2 && isLeapYear(year)){
+            return day+1;
+        }
+        return day;
+    }
+
+    public static int getDayInYear(int year) {
+        if(isLeapYear(year)){
+            return 366;
+        }
+        return 365;
     }
 }
---
+++ src/test/java/DateUtilsTest.java
@@ -0,0 +1,57 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.util.Date;
+
+public class DateUtilsTest {
+    @Test
+    public void test_convertToDate(){
+        String input="20/02/2017";
+        Date date = null;
+        DateUtils.sdf.setLenient(false);
+        try {
+            date = DateUtils.sdf.parse(input);
+        } catch (ParseException p) {
+        }
+        assertEquals(new DateUtils().convertToDate("20/02/2017",DateUtils.sdf),date);
+    }
+
+    @Test
+    public void test_isYear(){
+        assertEquals(new DateUtils().isYear(2017),true);
+        assertEquals(new DateUtils().isYear(1888),false);
+    }
+
+    @Test
+    public void test_isMonth(){
+        assertEquals(new DateUtils().isMonth(7),true);
+        assertEquals(new DateUtils().isYear(13),false);
+    }
+
+    @Test
+    public void test_isDay(){
+        assertEquals(new DateUtils().isDay(26,31),true);
+        assertEquals(new DateUtils().isDay(98,30),false);
+    }
+
+    @Test
+    public void test_isLeapYear(){
+        assertEquals(new DateUtils().isLeapYear(2000),true);
+        assertEquals(new DateUtils().isLeapYear(2015),false);
+    }
+
+    @Test
+    public void test_getDayInMonth(){
+        assertEquals(new DateUtils().getDayInMonth(2,2000),29);
+        assertEquals(new DateUtils().getDayInMonth(3,2000),31);
+        assertEquals(new DateUtils().getDayInMonth(2,2001),28);
+    }
+
+    @Test
+    public void test_getDayInYear(){
+        assertEquals(new DateUtils().getDayInYear(2017),365);
+        assertEquals(new DateUtils().getDayInYear(2000),366);
+    }
+
+}
