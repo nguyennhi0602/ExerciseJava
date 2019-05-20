87f7ca8a3720b49bde51bff24c79ebff75e3134f
nhi <nguyenthinhi0602@gmail.com>
2019-05-20 13:56:40 +0700

    exercise 22

--- src/main/java/Exercise22.java
+++ src/main/java/Exercise22.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Exercise22 {
+    public int numberDaysOfYearRemaining(String input) {
+        int remainDays = 0;
+        if (DateUtils.isDate(input) == false) {
+            throw new RuntimeException("Invalid date");
+        }
+        int day = DateUtils.getDay(input);
+        int month = DateUtils.getMonth(input);
+        int year = DateUtils.getYear(input);
+        int dayInYear = 0;
+        for (int i = 1; i < month; i++) {
+            dayInYear += DateUtils.getDayInMonth(i, year);
+        }
+        remainDays = DateUtils.getDayInYear(year) - dayInYear - day;
+        return remainDays;
+    }
 }
--- src/test/java/Exercise22Test.java
+++ src/test/java/Exercise22Test.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class Exercise22Test {
+    @Test
+    public void test_numberDaysOfYearRemaining(){
+        assertEquals(new Exercise22().numberDaysOfYearRemaining("12/02/2019"),322);
+    }
+    @Test(RuntimeException)
+    public void test_numberDaysOfYearRemaining(){
+        assertEquals(new Exercise22().numberDaysOfYearRemaining("12/02/2019"),0);
+    }
+
 }
