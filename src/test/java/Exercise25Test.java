cf4d6a8602c13064a946ed0957403859307d291d
nhi <nguyenthinhi0602@gmail.com>
2019-05-20 09:28:08 +0700

    exercise 25 edit

--- src/test/java/Exercise25Test.java
+++ src/test/java/Exercise25Test.java
@@ -5,7 +5,7 @@ public class Exercise25Test {
     @Test
     public void test_countFibonaciNumber(){
         assertEquals(new Exercise25().countFibonaciNumber(2,0),0);
-        //assertEquals(new Exercise25().countFibonaciNumber(2,3),2);
+        assertEquals(new Exercise25().countFibonaciNumber(2,3),2);
         assertEquals(new Exercise25().countFibonaciNumber(1,34),9);
 
     }
