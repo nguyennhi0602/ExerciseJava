30bda44f806b92ad9dcb860c89a9ea84ecfa2709
nhi <nguyenthinhi0602@gmail.com>
2019-05-21 10:01:40 +0700

    exercise 25 edit 3

--- src/main/java/Exercise25.java
+++ src/main/java/Exercise25.java
@@ -2,10 +2,9 @@ public class Exercise25 {
     public int countFibonaciNumber(int m, int n) {
         int a0 = 0;
         int a1 = 1;
-        int a2 = 1;
         int count = 0;
         while (a1 <= n) {
-            a2 = a0 + a1;
+            int a2 = a0 + a1;
             a0 = a1;
             a1 = a2;
             if (a0 >= m) {
