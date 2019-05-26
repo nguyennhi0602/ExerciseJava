02702ddc07ff718a95ef503063d5eaa31e540f58
nhi <nguyenthinhi0602@gmail.com>
2019-05-26 21:35:13 +0700

    exercise 27 edit

--- src/main/java/Exercise27.java
+++ src/main/java/Exercise27.java
@@ -1,17 +1,8 @@
-import java.math.BigInteger;
-
 public class Exercise27 {
     public int countZeroNumber(int number) {
-        BigInteger temp = new BigInteger("1");
-        for (int i = 1; i <= number; i++) {
-            temp = temp.multiply(new BigInteger(""+i));
-        }
         int count = 0;
-        String str = temp.toString();
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i)=='0') {
-                count++;
-            }
+        for (int i = 5; i <= number; i *= 5) {
+            count += number / i;
         }
         return count;
     }
--- src/test/java/Exercise27Test.java
+++ src/test/java/Exercise27Test.java
@@ -5,8 +5,8 @@ import static org.junit.Assert.assertEquals;
 public class Exercise27Test {
     @Test
     public void test_countZeroNumber() {
-        assertEquals(new Exercise27().countZeroNumber(12), 4);
-        assertEquals(new Exercise27().countZeroNumber(100), 30);
+        assertEquals(new Exercise27().countZeroNumber(10), 2);
+        assertEquals(new Exercise27().countZeroNumber(100), 24);
         assertEquals(new Exercise27().countZeroNumber(4), 0);
     }
 }
