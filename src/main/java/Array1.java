c5537376c3e85a3d4f8a4cbd38e895e9a0004a16
nhi <nguyenthinhi0602@gmail.com>
2019-05-27 09:43:08 +0700

    array 1

---
+++ src/main/java/Array1.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Array1 {
+    public List<Integer> findDuplicateElement(int[] arr) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            int count = 0;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+                if (count > 1 && !result.contains(arr[i])) {
+                    result.add(arr[i]);
+                }
+            }
+        }
+        return result;
+    }
+}
