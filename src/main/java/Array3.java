dd258e6e2668428a380758574c8412924d330414
nhi <nguyenthinhi0602@gmail.com>
2019-05-28 18:35:07 +0700

    array 3 edit

--- src/main/java/Array3.java
+++ src/main/java/Array3.java
@@ -1,13 +1,9 @@
 public class Array3 {
     public int calculate(int[] arr) {
         int sum = 0;
-        int sumEvenNumber = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % 2 == 0) {
-                sumEvenNumber += arr[i];
-            }
-            sum += arr[i];
+            sum += (1 - (arr[i] & 1 * 2)) * arr[i];
         }
-        return 2 * sumEvenNumber - sum;
+        return sum;
     }
 }
