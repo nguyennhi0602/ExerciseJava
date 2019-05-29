224946b81c2e51900d99872079edc452c9ca650f
nhi <nguyenthinhi0602@gmail.com>
2019-05-29 09:58:24 +0700

    array 4 edit

--- src/main/java/Array4.java
+++ src/main/java/Array4.java
@@ -1,30 +1,24 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Array4 {
-    public List<Integer> sortArray(int[] arr) {
-        List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % 2 == 0) {
-                result.add(0, arr[i]);
-            } else {
-                result.add(result.size(), arr[i]);
-            }
-        }
-        for (int i = 0; i < result.size() - 1; i++) {
-            for (int j = i + 1; j < result.size(); j++) {
-                if (result.get(i) % 2 == 0 && result.get(j) % 2 == 0 && (result.get(i) > result.get(j))) {
-                    int temp = result.get(i);
-                    result.set(i, result.get(j));
-                    result.set(j, temp);
+    public int[] sortArray(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] % 2 != 0 && arr[j] % 2 == 0) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
-                if (result.get(i) % 2 != 0 && result.get(j) % 2 != 0 && (result.get(i) < result.get(j))) {
-                    int temp = result.get(i);
-                    result.set(i, result.get(j));
-                    result.set(j, temp);
+                if (arr[i] % 2 == 0 && arr[j] % 2 == 0 && arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+                if (arr[i] % 2 != 0 && arr[j] % 2 != 0 && arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
-        return result;
+        return arr;
     }
 }
--- src/test/java/Array4Test.java
+++ src/test/java/Array4Test.java
@@ -1,13 +1,10 @@
+import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.Arrays;
-
-import static org.junit.Assert.assertEquals;
 
 public class Array4Test {
     @Test
     public void test_sortArray() {
-        assertEquals(new Array4().sortArray(new int[]{1, 3, 2, 6, 5, 4, 5, 1, 2}), Arrays.asList(2, 2, 4, 6, 5, 5, 3, 1, 1));
+        Assert.assertArrayEquals(new Array4().sortArray(new int[]{1, 3, 4, 2, 4, 5}), new int[]{2, 4, 4, 5, 3, 1});
     }
 
 }
