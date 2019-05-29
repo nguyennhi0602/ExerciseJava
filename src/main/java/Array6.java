33e48032a03706ab0022f6aab51e65441744fab2
nhi <nguyenthinhi0602@gmail.com>
2019-05-29 10:09:54 +0700

    array 6

---
+++ src/main/java/Array6.java
@@ -0,0 +1,20 @@
+public class Array6 {
+
+    public int[] sortArray(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] % 2 == 0 && arr[j] % 2 == 0 && arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+                if (arr[i] % 2 != 0 && arr[j] % 2 != 0 && arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
