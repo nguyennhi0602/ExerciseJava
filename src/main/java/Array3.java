c135c8eccdc77868dcb1b6496cdce53e139382b1
nhi <nguyenthinhi0602@gmail.com>
2019-05-29 08:52:23 +0700

    array 3 edit

--- src/main/java/Array3.java
+++ src/main/java/Array3.java
@@ -2,7 +2,7 @@ public class Array3 {
     public int calculate(int[] arr) {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
-            sum += (1 - (arr[i] & 1 * 2)) * arr[i];
+            sum += 2 * ((1 - arr[i] & 1) * arr[i]) - arr[i];
         }
         return sum;
     }
--- src/test/java/Array3Test.java
+++ src/test/java/Array3Test.java
@@ -5,5 +5,6 @@ public class Array3Test {
     @Test
     public void test_calculate(){
         assertEquals(new Array3().calculate(new int[]{2,3,4,5,1,2,4,2}),5);
+        assertEquals(new Array3().calculate(new int[]{2,6,4,5,1}),6);
     }
 }
