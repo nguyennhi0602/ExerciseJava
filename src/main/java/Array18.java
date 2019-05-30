f4bb7cfee79592affe9a611664bbf1c48776b155
nhi <nguyenthinhi0602@gmail.com>
2019-05-30 20:37:44 +0700

    array 18

--- src/main/java/Array18.java
+++ src/main/java/Array18.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Array18 {
     public int[][] sortArray(int[][] arr) {
         int[] temp = new int[arr.length * arr[0].length];
@@ -17,13 +19,15 @@ public class Array18 {
                 }
             }
         }
-        System.out.println(index);
+        System.out.println(Arrays.toString(temp));
         index = 0;
         for (int i = 0; i < arr.length; i++) {
-            for (int j = arr[0].length - 1; j >= 0; j++) {
-                arr[i][j] = temp[index];
-                index++;
+            for (int j = arr[0].length - 1; j >= 0; j--) {
+                arr[i][j] = temp[index++];
+                System.out.print(arr[i][j]+"  ");
+                //index++;
             }
+            System.out.println();
         }
         return arr;
     }
