a6e1474dda2edcb66e00b513d6fd183f32108dc1
nhi <nguyenthinhi0602@gmail.com>
2019-05-30 15:15:59 +0700

    array 20

--- src/test/java/Array20Test.java
+++ src/test/java/Array20Test.java
@@ -11,9 +11,9 @@ public class Array20Test {
                 new int[][]{{8, 9, 10},
                         {14, 15, 16}});
         Assert.assertArrayEquals(new Array20().getSubMatrix(
-                new int[][]{{1, 2, 3, 4, 5, 6},
-                        {7, 8, 9, 10, 11, 12},
-                        {13, 14, 15, 16, 16, 17}}, 4, 1, 1, 3),
+                new int[][]{{1, 2, 3, 4, 5},
+                        {7, 8, 9, 10, 11},
+                        {13, 14, 15, 16, 17}}, 4, 1, 1, 3),
                 new int[][]{});
     }
 }
