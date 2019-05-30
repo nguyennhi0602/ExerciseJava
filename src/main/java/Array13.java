5a3aa0f2087a25bfe836e3f90a6a26a597afff6c
nhi <nguyenthinhi0602@gmail.com>
2019-05-30 10:24:07 +0700

    array 13 edit

--- src/main/java/Array13.java
+++ src/main/java/Array13.java
@@ -2,8 +2,10 @@ public class Array13 {
     public int[][] sortArray(int[][] arr) {
         int[][] result = new int[arr[0].length][arr.length];
         for(int i=0;i<arr[0].length;i++){
+            int col=0;
             for (int j = arr.length - 1; j >= 0; j--) {
-                result[i][result.length - j] = arr[j][i];
+                result[i][col] = arr[j][i];
+                col++;
             }
         }
         return result;
