0337687f18a0e096d2b125d4313158cd49fd3163
nhi <nguyenthinhi0602@gmail.com>
2019-05-30 10:58:46 +0700

    array 10 edit 2

--- src/main/java/Array10.java
+++ src/main/java/Array10.java
@@ -1,20 +1,17 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Array10 {
-    public List<List<Integer>> minusArray(int[] arr, int n) {
+    public int[][] minusArray(int[] arr, int n) {
         int mid = (int) Math.ceil((double) arr.length / n);
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            temp.add(arr[i]);
-            if (temp.size() == mid) {
-                result.add(temp);
-                temp = new ArrayList<>();
+        int[][] result = new int[n][mid];
+        int index = 0;
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+                if (index >= arr.length) {
+                    result[i][j] = 0;
+                } else {
+                    result[i][j] = arr[index];
+                }
+                index++;
             }
-        }
-        if (result.size() < n) {
-            result.add(temp);
         }
         return result;
     }
