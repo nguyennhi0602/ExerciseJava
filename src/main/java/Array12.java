b8d5865a733bce9a8171a533d5d2b41bed69c325
nhi <nguyenthinhi0602@gmail.com>
2019-05-30 09:07:15 +0700

    array 12

--- src/main/java/Array12.java
+++ src/main/java/Array12.java
@@ -1,21 +1,17 @@
 public class Array12 {
     public int[] sortArray(int[] arr, int n) {
-        int index = arr.length - 1;
+        int index = 0;
         int[] result = new int[arr.length + 1];
-        boolean inserted = false;
-        for (int i = arr.length; i >= 0; i--) {
-            if (arr[index] > n) {
-                result[i] = arr[index--];
-            } else {
-                if (!inserted) {
-                    result[i] = n;
-                    inserted = true;
-                } else {
-                    result[i] = arr[index--];
-                }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= n) {
+                result[i] = arr[i];
+                index = i + 1;
             }
+        }
+        result[index] = n;
+        for (int i = index + 1; i < result.length; i++) {
+            result[i] = arr[i - 1];
         }
         return result;
     }
-
 }
