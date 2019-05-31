df143e80f3e83ff524b7ef5ff17dfc57fd662acf
nhi <nguyenthinhi0602@gmail.com>
2019-05-31 14:49:51 +0700

    array 24

--- src/main/java/Array24.java
+++ src/main/java/Array24.java
@@ -1,5 +1,5 @@
 public class Array24 {
-    public int caculateGCD(int a, int b) {
+    public int caculateLGM(int a, int b) {
         int temp = a * b;
         if (a == 0 || b == 0) {
             return (a * b) / (a + b);
@@ -14,10 +14,10 @@ public class Array24 {
         return temp / a;
     }
 
-    public int calculateGCDOfArray(int[] arr) {
-        int result = caculateGCD(arr[0], arr[1]);
+    public int calculateLGMOfArray(int[] arr) {
+        int result = caculateLGM(arr[0], arr[1]);
         for (int i = 2; i < arr.length; i++) {
-            result = caculateGCD(result, arr[i]);
+            result = caculateLGM(result, arr[i]);
         }
         return result;
     }
--- src/test/java/Array24Test.java
+++ src/test/java/Array24Test.java
@@ -5,13 +5,13 @@ import static org.junit.Assert.assertEquals;
 public class Array24Test {
     @Test
     public void test_caculateGCD() {
-        assertEquals(new Array24().caculateGCD(2, 0), 0);
-        assertEquals(new Array24().caculateGCD(2, 3), 6);
+        assertEquals(new Array24().caculateLGM(2, 0), 0);
+        assertEquals(new Array24().caculateLGM(2, 3), 6);
     }
 
     @Test
     public void test_calculateGCDOfArray() {
-        assertEquals(new Array24().calculateGCDOfArray(new int[]{1, 2, 3}), 6);
-        assertEquals(new Array24().calculateGCDOfArray(new int[]{0, 2, 3}), 0);
+        assertEquals(new Array24().calculateLGMOfArray(new int[]{1, 2, 3}), 6);
+        assertEquals(new Array24().calculateLGMOfArray(new int[]{0, 2, 3}), 0);
     }
 }
