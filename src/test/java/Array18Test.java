c66612cc5ab43fbb5b67578aba8547fe38c88eca
nhi <nguyenthinhi0602@gmail.com>
2019-05-31 16:12:16 +0700

    array 18 edit

--- src/test/java/Array18Test.java
+++ src/test/java/Array18Test.java
@@ -11,5 +11,12 @@ public class Array18Test {
                 new int[][]{{2, 2, 1},
                             {4, 4, 3},
                             {7, 6, 5}});
+        Assert.assertArrayEquals(new Array18().sortArray(
+                new int[][]{{5, 6, 4, 3},
+                        {1, 2, 3, 4},
+                        {4, 2, 7, 8}}),
+                new int[][]{{3, 2, 2, 1},
+                        {4, 4, 4, 3},
+                        {8, 7, 6, 5}});
     }
 }
