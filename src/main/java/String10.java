e670a6e5eb1abae20dde1735f2f91c67685b6c5a
nhi <nguyenthinhi0602@gmail.com>
2019-06-04 13:41:14 +0700

    string 10

--- src/main/java/String10.java
+++ src/main/java/String10.java
@@ -1,15 +1,22 @@
 public class String10 {
     public String getStringIsValid(String str) {
         String result = "";
+        int count = 0;
         for (int i = 0; i < str.length(); i++) {
             if (Character.isDigit(str.charAt(i))) {
-                int count = Integer.parseInt(String.valueOf(str.charAt(i))) - 1;
-                while (count != 0) {
-                    result += String.valueOf(str.charAt(i - 1));
-                    count--;
+                int count1 = -count;
+                count = count * 10 + Integer.parseInt(String.valueOf(str.charAt(i)));
+                count1 += count;
+                if (Character.isDigit(str.charAt(i + 1)) == false) {
+                    count1 += -1;
+                }
+                while (count1 != 0) {
+                    result += String.valueOf(result.charAt(result.length() - 1));
+                    count1--;
                 }
             } else {
                 result += String.valueOf(str.charAt(i));
+                count = 0;
             }
         }
         return result;
--- src/test/java/String10Test.java
+++ src/test/java/String10Test.java
@@ -5,5 +5,6 @@ public class String10Test {
     @Test
     public void test_getStringIsValid() {
         Assert.assertEquals(new String10().getStringIsValid("abc4e6fd"), "abcccceeeeeefd");
+        Assert.assertEquals(new String10().getStringIsValid("ab13c"), "abbbbbbbbbbbbbc");
     }
 }
