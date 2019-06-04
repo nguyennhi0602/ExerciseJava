c0f6af7934d81b19ca4a7b1e3ebafddf6688b5fe
nhi <nguyenthinhi0602@gmail.com>
2019-06-04 14:40:24 +0700

    string 13

--- src/main/java/String13.java
+++ src/main/java/String13.java
@@ -1,3 +1,11 @@
 public class String13 {
-    
+    public String getStringIsValid(String str1,String str2){
+        String result="";
+        for(int i=0;i<str1.length();i++){
+        }
+        return result;
+    }
 }
+
+//nguyen thi nhi nhi nguyen
+//haha nguyen nhi akjshj
