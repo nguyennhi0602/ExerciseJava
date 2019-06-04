11a7ad19bb982e6a7528c4663dac549aba9a8c48
nhi <nguyenthinhi0602@gmail.com>
2019-06-04 16:58:50 +0700

    string 13

--- src/main/java/String13.java
+++ src/main/java/String13.java
@@ -1,11 +1,23 @@
 public class String13 {
-    public String getStringIsValid(String str1,String str2){
+    public String deleteDuplicateElement(String str) {
         String result="";
-        for(int i=0;i<str1.length();i++){
+        for (int i = 0; i < str.length(); i++) {
+            if (!result.contains(String.valueOf(str.charAt(i)))) {
+                result += String.valueOf(str.charAt(i));
+            }
         }
         return result;
     }
-}
 
-//nguyen thi nhi nhi nguyen
-//haha nguyen nhi akjshj
+    public boolean getStringIsValid(String str1, String str2) {
+        str1 = deleteDuplicateElement(str1);
+        str2 = deleteDuplicateElement(str2);
+        boolean check = true;
+        for (int i = 0; i < str1.length(); i++) {
+            if (!str2.contains(String.valueOf(str1.charAt(i)))) {
+                check = false;
+            }
+        }
+        return check;
+    }
+}
