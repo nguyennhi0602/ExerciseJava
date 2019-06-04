f5a27cd6eabf508beb19778158aed1965da09bcb
nhi <nguyenthinhi0602@gmail.com>
2019-06-04 09:39:53 +0700

    string 8

--- src/test/java/String8Test.java
+++ src/test/java/String8Test.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 public class String8Test {
     @Test
     public void test_standardizeString(){
-        Assert.assertEquals(new String8().standardizeString("nguyen thi nhi.lop 15T3;truong dhbk Da   nang"),"");
+        Assert.assertEquals(new String8().standardizeString("nguyen thi nhi.lop 15T3;truong dhbk Da   nang"),
+                "Nguyen thi nhi. Lop 15T3; Truong dhbk Da nang");
     }
 }
