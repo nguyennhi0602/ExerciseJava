d77b45632e81c0233c5b0818a846d81965be2416
nhi <nguyenthinhi0602@gmail.com>
2019-06-05 10:27:12 +0700

    string 15 edit

--- src/main/java/String15.java
+++ src/main/java/String15.java
@@ -4,22 +4,14 @@ public class String15 {
         int oddNumber = 0;
         for (int i = 0; i < str.length(); i++) {
             if (i % 2 == 0) {
-                System.out.println( str.charAt(i));
-                oddNumber += Character.getNumericValue(str.charAt(i));
-                System.out.println(oddNumber+","+str.charAt(i));
+                oddNumber += str.charAt(i) - '0';
             } else {
-                evenNumber += str.charAt(i)-0;
+                evenNumber += str.charAt(i) - '0';
             }
         }
-        System.out.println(oddNumber);
-        int sum = oddNumber + 3 * evenNumber;
-        System.out.println(sum);
-        if (sum % 10 != 0) {
-            return 10 - (sum % 10);
+        if ((oddNumber + 3 * evenNumber) % 10 != 0) {
+            return 10 - (oddNumber + 3 * evenNumber) % 10;
         }
-//        return 0;
-        System.out.println(oddNumber + 3 * evenNumber);
-        System.out.println((oddNumber + 3 * evenNumber)%10);
-        return 10-(oddNumber + 3 * evenNumber)%10;
+        return 0;
     }
 }
