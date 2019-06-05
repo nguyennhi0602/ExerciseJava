7a4b4c1f8d354c9c33510ef91a86113eb4f77e7a
nhi <nguyenthinhi0602@gmail.com>
2019-06-05 10:56:16 +0700

    string 10

--- src/main/java/String10.java
+++ src/main/java/String10.java
@@ -1,24 +1,20 @@
 public class String10 {
     public String getStringIsValid(String str) {
-        String result = "";
-        int count = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (Character.isDigit(str.charAt(i))) {
-                int count1 = -count;
-                count = count * 10 + Integer.parseInt(String.valueOf(str.charAt(i)));
-                count1 += count;
-                if (Character.isDigit(str.charAt(i + 1)) == false) {
-                    count1 += -1;
-                }
-                while (count1 != 0) {
-                    result += String.valueOf(result.charAt(result.length() - 1));
-                    count1--;
-                }
-            } else {
-                result += String.valueOf(str.charAt(i));
-                count = 0;
+        StringBuffer result = new StringBuffer();
+        int i = 0;
+        while (i < str.length()) {
+            char temp = str.charAt(i);
+            result.append(temp);
+            i++;
+            String number = "0";
+            while (i < str.length() && Character.isDigit(str.charAt(i))) {
+                number += str.charAt(i);
+                i++;
+            }
+            for (int j = 1; j < Integer.valueOf(number); j++) {
+                result.append(temp);
             }
         }
-        return result;
+        return result.toString();
     }
 }
