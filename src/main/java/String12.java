4d4c4f43a2f9e74663aac702e4e185c4225d0d0b
nhi <nguyenthinhi0602@gmail.com>
2019-06-05 09:07:05 +0700

    string 12 edit

--- src/main/java/String12.java
+++ src/main/java/String12.java
@@ -1,13 +1,13 @@
 public class String12 {
     public String deleteDuplicateElement(String str){
-        String result = "";
+        StringBuffer result = new StringBuffer();
         str = str + " ";
         for (int i = 0; i < str.length() - 1; i++) {
             if (str.charAt(i) == str.charAt(i + 1)) {
                 continue;
             }
-            result += String.valueOf(str.charAt(i));
+            result.append(str.charAt(i));
         }
-        return result;
+        return result.toString();
     }
 }
--- src/test/java/String12Test.java
+++ src/test/java/String12Test.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class String12Test {
     @Test
     public void test_deleteDuplicateElement() {
-        Assert.assertEquals(new String12().deleteDuplicateElement("aaaaaabcaaa"), "abca");
+        Assert.assertEquals(new String12().deleteDuplicateElement("aaaaaabcaaad"), "abcad");
         Assert.assertEquals(new String12().deleteDuplicateElement("abbbbbbccccd eeffffddbc"), "abcd efdbc");
     }
 }
