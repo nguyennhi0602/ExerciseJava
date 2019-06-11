972f50cd2a88d3fcc3e3ccff5bc530cec9b4d0e4
nhi <nguyenthinhi0602@gmail.com>
2019-06-11 10:53:40 +0700

    oop1

---
+++ src/main/java/Oop1.java
@@ -0,0 +1,65 @@
+public class Oop1 {
+    private double a1;
+    private double a2;
+    private double b1;
+    private double b2;
+    private double c1;
+    private double c2;
+    
+    public Oop1(double a1, double a2, double b1, double b2, double c1, double c2) {
+        this.a1 = a1;
+        this.a2 = a2;
+        this.b1 = b1;
+        this.b2 = b2;
+        this.c1 = c1;
+        this.c2 = c2;
+    }
+
+    public double getA1() {
+        return a1;
+    }
+
+    public void setA1(double a1) {
+        this.a1 = a1;
+    }
+
+    public double getA2() {
+        return a2;
+    }
+
+    public void setA2(double a2) {
+        this.a2 = a2;
+    }
+
+    public double getB1() {
+        return b1;
+    }
+
+    public void setB1(double b1) {
+        this.b1 = b1;
+    }
+
+    public double getB2() {
+        return b2;
+    }
+
+    public void setB2(double b2) {
+        this.b2 = b2;
+    }
+
+    public double getC1() {
+        return c1;
+    }
+
+    public void setC1(double c1) {
+        this.c1 = c1;
+    }
+
+    public double getC2() {
+        return c2;
+    }
+
+    public void setC2(double c2) {
+        this.c2 = c2;
+    }
+}
