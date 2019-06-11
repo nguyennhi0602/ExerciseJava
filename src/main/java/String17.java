5d50b18c78cadef9440f5c5493f0e1eba8c707a5
nhi <nguyenthinhi0602@gmail.com>
2019-06-11 09:33:36 +0700

    string 17

--- src/main/java/String17.java
+++ src/main/java/String17.java
@@ -1,34 +1,47 @@
 public class String17 {
-    public String calculate(String num1, String num2) {
-        StringBuffer result = new StringBuffer();
-        int temp = num1.length() - num2.length();
-        int suplus = 0;
-        int minus = 0;
-        for (int i = num1.length() - 1; i >= 0; i--) {
-            if (i - temp >= 0) {
-                minus = (num1.charAt(i) - '0') - (num2.charAt(i - temp) - '0') + suplus;
-                if ((num1.charAt(i) - '0') < (num2.charAt(i - temp) - '0')) {
-                    minus = minus + 10;
-                    result.insert(0, minus);
-                    suplus = -1;
-                } else {
-                    result.insert(0, minus);
-                    suplus = 0;
-                }
-            } else {
-                minus = num1.charAt(i) - '0' + suplus;
-                if (num1.charAt(i) == '0') {
-                    minus += 10;
-                    suplus = -1;
-                } else {
-                    suplus = 0;
-                }
-                result.insert(0, minus);
+    public boolean isBiger(String a, String b) {
+        if (a.length() != b.length()) {
+            return a.length() > b.length();
+        }
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                return a.charAt(i) > b.charAt(i);
             }
         }
-        if (result.charAt(0) == '0') {
-            return result.toString().substring(1);
+        return false;
+    }
+
+    public String calculate(String a, String b, int index, int buff) {
+        int f = 0;
+        int s = 0;
+        int indexA = a.length() - index;
+        int indexB = b.length() - index;
+        if (indexA >= 0) {
+            f = a.charAt(indexA) - '0';
         }
-        return result.toString();
+        if (indexB >= 0) {
+            s = b.charAt(indexB) - '0';
+        }
+        if (indexA <= 0 && indexB <= 0) {
+            return (String.valueOf(f - buff - s));
+        }
+        return calculate(a, b, ++index, (f - buff - s) < 0 ? 1 : 0) + String.valueOf((f - buff - s) < 0 ? (f + 10 - buff - s) : (f - s));
+    }
+
+    public String sub(String a, String b) {
+        String big = isBiger(a, b) ? a : b;
+        String small = isBiger(a, b) ? b : a;
+        return ((isBiger(a, b) ? "" : "-") + calculate(big, small, 1, 0));
+    }
+
+    public String calculateResult(String a, String b) {
+        String result = sub(a, b);
+        int index = 0;
+        while (result.charAt(index) == '0') {
+            result = result.substring(++index);
+            index--;
+        }
+        return result;
     }
 }
+
--- src/test/java/String17Test.java
+++ src/test/java/String17Test.java
@@ -3,9 +3,19 @@ import org.junit.Test;
 
 public class String17Test {
     @Test
-    public void test_calculate() {
-        Assert.assertEquals(new String17().calculate("123", "111"), "12");
-        Assert.assertEquals(new String17().calculate("10000045", "124678"), "9875367");
-        Assert.assertEquals(new String17().calculate("10000045", "1232"), "9998813");
+    public void test_isBiger() {
+        Assert.assertEquals(new String17().isBiger("10000045", "124678"), true);
+        Assert.assertEquals(new String17().isBiger("12", "1232"), false);
+    }
+
+    @Test
+    public void test_sub() {
+        Assert.assertEquals(new String17().sub("10000045", "124678"), "09875367");
+    }
+
+    @Test
+    public void test_calculateResult() {
+        Assert.assertEquals(new String17().calculateResult("10000045", "124678"), "9875367");
+        Assert.assertEquals(new String17().calculateResult("10000045", "1232"), "9998813");
     }
 }
