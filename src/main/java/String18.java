f76105cf73bf7414c416060ef2e502e184896fc3
nhi <nguyenthinhi0602@gmail.com>
2019-06-11 16:00:21 +0700

    string 18 edit

--- src/main/java/String18.java
+++ src/main/java/String18.java
@@ -1,13 +1,13 @@
 public class String18 {
 
-    public String multiply(String a, String b) {
-        if (b.length() != 1) {
+    public String multiply(String a, int b) {
+        if (b > 9) {
             return "";
         }
         StringBuffer result = new StringBuffer();
         int buff = 0;
         for (int i = a.length() - 1; i >= 0; i--) {
-            int mult = (a.charAt(i) - '0') * Integer.parseInt(b) + buff;
+            int mult = (a.charAt(i) - '0') * b + buff;
             result.insert(0, mult % 10);
             buff = mult / 10;
         }
@@ -20,7 +20,7 @@ public class String18 {
     public String calculate(String num1, String num2) {
         String result="";
         for(int i=num2.length()-1;i>=0;i--){
-            String temp=multiply(num1,num2.charAt(i)+"");
+            String temp = multiply(num1, num2.charAt(i) - '0');
             int count=num2.length()-1-i;
             while (count!=0){
                 temp+="0";
--- src/test/java/String18Test.java
+++ src/test/java/String18Test.java
@@ -4,9 +4,9 @@ import org.junit.Test;
 public class String18Test {
     @Test
     public void test_multiply(){
-        Assert.assertEquals(new String18().multiply("123","2"),"246");
-        Assert.assertEquals(new String18().multiply("458","7"),"3206");
-        Assert.assertEquals(new String18().multiply("10002","7"),"70014");
+        Assert.assertEquals(new String18().multiply("123", 2), "246");
+        Assert.assertEquals(new String18().multiply("458", 7), "3206");
+        Assert.assertEquals(new String18().multiply("10002", 7), "70014");
     }
 
     @Test
